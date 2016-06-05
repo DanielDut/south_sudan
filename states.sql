a84c250d93a17b5561ffcab438b9065b01ff0128
Daniel Dut A <d.dut@live.com>
2016-06-05 05:06:29 -0400

    Adds Counties, modifies states

--- states.sql
+++ states.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS `states` (
   `capital` varchar(80) DEFAULT NULL,
   `region` varchar(80) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=29 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 --
 -- Data for table `states`
@@ -15,31 +15,31 @@ CREATE TABLE IF NOT EXISTS `states` (
 
 INSERT INTO `states` (`name`, `capital`, `region`) VALUES 
 ('Amadi', 'Mundri', 'Equatoria'), 
-('Aweil', 'Aweil', 'Bahr el Ghazal'), 
-('Aweil East', 'Wanyjok', 'Bahr el Ghazal'), 
+('Aweil', 'Aweil', 'Bahr El Ghazal'), 
+('Aweil East', 'Wanyjok', 'Bahr El Ghazal'), 
 ('Boma', 'Pibor', 'Upper Nile'), 
 ('Eastern Bieh', 'Akobo', 'Upper Nile'), 
-('Eastern Lakes', 'Yirol', 'Bahr el Ghazal'), 
+('Eastern Lakes', 'Yirol', 'Bahr El Ghazal'), 
 ('Eastern Nile', 'Malakal', 'Upper Nile'), 
 ('Gbudwe', 'Yambio', 'Equatoria'), 
-('Gogrial', 'Kuacjok', 'Bahr el Ghazal'), 
-('Gok', 'Cueibet', 'Bahr el Ghazal'), 
+('Gogrial', 'Kuacjok', 'Bahr El Ghazal'), 
+('Gok', 'Cueibet', 'Bahr El Ghazal'), 
 ('Imatong', 'Torit', 'Equatoria'), 
 ('Jonglei', 'Bor', 'Upper Nile'), 
 ('Jubek', 'Juba', 'Equatoria'), 
 ('Latjoor', 'Nasir', 'Upper Nile'), 
-('Lol', 'Raga', 'Bahr el Ghazal'), 
+('Lol', 'Raga', 'Bahr El Ghazal'), 
 ('Maridi', 'Maridi', 'Equatoria'), 
 ('Namorunyang', 'Kapoeta', 'Equatoria'), 
 ('Northern Liech', 'Bentiu', 'Upper Nile'), 
 ('Ruweng', 'Panriang', 'Upper Nile'), 
 ('Southern Liech', 'Leer', 'Upper Nile'), 
 ('Terekeka', 'Terekeka', 'Equatoria'), 
-('Tonj', 'Tonj', 'Bahr el Ghazal'), 
-('Twic', 'Mayen Abun', 'Bahr el Ghazal'), 
-('Wau', 'Wau', 'Bahr el Ghazal'), 
+('Tonj', 'Tonj', 'Bahr El Ghazal'), 
+('Twic', 'Mayen Abun', 'Bahr El Ghazal'), 
+('Wau', 'Wau', 'Bahr El Ghazal'), 
 ('Western Bieh', 'Ayod', 'Upper Nile'), 
-('Western Lakes', 'Rumbek', 'Bahr el Ghazal'), 
+('Western Lakes', 'Rumbek', 'Bahr El Ghazal'), 
 ('Western Nile', 'Kodok', 'Upper Nile'), 
 ('Yei River', 'Yei', 'Equatoria'); 
 
