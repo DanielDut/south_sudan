b750f7d1ce34ea65aaeac4ea967ce491f8e81cdf
Daniel Dut A <d.dut@live.com>
2017-01-15 17:57:00 -0500

    Modifies states

--- states.sql
+++ states.sql
@@ -1,4 +1,6 @@
 
+-- South Sudan --
+
 --  `states` table structure
 
 CREATE TABLE IF NOT EXISTS `states` (
@@ -7,39 +9,43 @@ CREATE TABLE IF NOT EXISTS `states` (
   `capital` varchar(80) DEFAULT NULL,
   `region` varchar(80) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
 
 --
 -- Data for table `states`
 --
 
 INSERT INTO `states` (`name`, `capital`, `region`) VALUES 
-('Amadi', 'Mundri', 'Equatoria'), 
-('Aweil', 'Aweil', 'Bahr El Ghazal'), 
-('Aweil East', 'Wanyjok', 'Bahr El Ghazal'), 
-('Boma', 'Pibor', 'Upper Nile'), 
-('Eastern Bieh', 'Akobo', 'Upper Nile'), 
-('Eastern Lakes', 'Yirol', 'Bahr El Ghazal'), 
-('Eastern Nile', 'Malakal', 'Upper Nile'), 
-('Gbudwe', 'Yambio', 'Equatoria'), 
-('Gogrial', 'Kuacjok', 'Bahr El Ghazal'), 
-('Gok', 'Cueibet', 'Bahr El Ghazal'), 
-('Imatong', 'Torit', 'Equatoria'), 
-('Jonglei', 'Bor', 'Upper Nile'), 
-('Jubek', 'Juba', 'Equatoria'), 
-('Latjoor', 'Nasir', 'Upper Nile'), 
-('Lol', 'Raga', 'Bahr El Ghazal'), 
-('Maridi', 'Maridi', 'Equatoria'), 
-('Namorunyang', 'Kapoeta', 'Equatoria'), 
-('Northern Liech', 'Bentiu', 'Upper Nile'), 
-('Ruweng', 'Panriang', 'Upper Nile'), 
-('Southern Liech', 'Leer', 'Upper Nile'), 
-('Terekeka', 'Terekeka', 'Equatoria'), 
-('Tonj', 'Tonj', 'Bahr El Ghazal'), 
-('Twic', 'Mayen Abun', 'Bahr El Ghazal'), 
-('Wau', 'Wau', 'Bahr El Ghazal'), 
-('Western Bieh', 'Ayod', 'Upper Nile'), 
-('Western Lakes', 'Rumbek', 'Bahr El Ghazal'), 
-('Western Nile', 'Kodok', 'Upper Nile'), 
-('Yei River', 'Yei', 'Equatoria'); 
-
+('Abyei Administrative Area', 'Abyei', 'Bahr El Ghazal'),
+('Akobo', 'Akobo', 'Upper Nile'),
+('Amadi', 'Mundri', 'Equatoria'),
+('Aweil', 'Aweil', 'Bahr El Ghazal'),
+('Aweil East', 'Wanyjok', 'Bahr El Ghazal'),
+('Bieh', 'Waat', 'Upper Nile'),
+('Boma', 'Pibor', 'Upper Nile'),
+('Central Upper Nile', 'Malakal', 'Upper Nile'),
+('Eastern Lakes', 'Yirol', 'Bahr El Ghazal'),
+('Fangak', 'Fangak', 'Upper Nile'),
+('Fashoda', 'Kodok', 'Upper Nile'),
+('Gbudwe', 'Yambio', 'Equatoria'),
+('Gogrial', 'Kuacjok', 'Bahr El Ghazal'),
+('Gok', 'Cueibet', 'Bahr El Ghazal'),
+('Imatong', 'Torit', 'Equatoria'),
+('Jonglei', 'Bor', 'Upper Nile'),
+('Jubek', 'Juba', 'Equatoria'),
+('Kapoeta', 'Kapoeta', 'Equatoria'),
+('Latjor', 'Nasir', 'Upper Nile'),
+('Lol', 'Raga', 'Bahr El Ghazal'),
+('Maiwut', 'Maiwut', 'Upper Nile'),
+('Maridi', 'Maridi', 'Equatoria'),
+('Northern Liech', 'Bentiu', 'Upper Nile'),
+('Northern Upper Nile', 'Renk', 'Upper Nile'),
+('Ruweng', 'Panriang', 'Upper Nile'),
+('Southern Liech', 'Leer', 'Upper Nile'),
+('Terekeka', 'Terekeka', 'Equatoria'),
+('Tombura', 'Tombura', 'Equatoria'),
+('Tonj', 'Tonj', 'Bahr El Ghazal'),
+('Twic', 'Mayen Abun', 'Bahr El Ghazal'),
+('Wau', 'Wau', 'Bahr El Ghazal'),
+('Western Lakes', 'Rumbek', 'Bahr El Ghazal'),
+('Yei River', 'Yei', 'Equatoria');
